7553d898f75f53e9d4bdc56d5f72125f00337118
Kyochul-Jang <jang128@purdue.edu>
2022-08-10 02:51:18 +0900

    Add

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,52 +1,60 @@
 import React from 'react'
 import { Carousel, Container } from 'react-bootstrap';
 import './Projects.css';
-import javaProject from './java-social-network-project.jpg'
+import javaProject from './java-social-network-project.jpg';
+import snuProject from './snu-dash-project.png';
+import oldWebProject from './old-website-project.png';
+
 
 
 export default function Projects() {
     return (
 
         <div className="container carousel-container">
-            <Carousel>
+            <div className="about-content project-title">
+                <h2 className='about-title'>Projects</h2>
+                <h2 className='about-title1'>Projects</h2>
+            </div>
+            <Carousel variant='dark'>
                 <Carousel.Item>
                     <img
-                        className="java-project"
+                        className="project"
                         src={javaProject}
                         alt="First slide"
-                        
+
                     />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    <Carousel.Caption className='label'>
+                    <h3 ><span className='label-title'>Third slide label</span></h3>
+                        <p className='stacks'>Stacks: Java</p>
+                        <h5>Built a social network platform for getting a job like LinkedIn. <br />Worked on testing apps by using JUnit Test.</h5>
                     </Carousel.Caption>
                 </Carousel.Item>
-                {/* <Carousel.Item>
+                <Carousel.Item>
                     <img
-                        className="d-block w-100"
-                        src="holder.js/800x400?text=Second slide&bg=282c34"
+                        className="project"
+                        src={snuProject}
                         alt="Second slide"
                     />
 
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
+                    <Carousel.Caption className='label'>
+                    <h3 ><span className='label-title'>Third slide label</span></h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                        className="d-block w-100"
-                        src="holder.js/800x400?text=Third slide&bg=20232a"
+                        className="project"
+                        src={oldWebProject}
                         alt="Third slide"
                     />
 
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
+                    <Carousel.Caption className='label'>
+                        <h3 ><span className='label-title'>Third slide label</span></h3>
                         <p>
                             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
                         </p>
                     </Carousel.Caption>
-                </Carousel.Item> */}
+                </Carousel.Item>
             </Carousel>
         </div>
     );
