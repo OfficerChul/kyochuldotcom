4f297356345ba7f53d56287c89e50c0a068c4519
Kyochul-Jang <jang128@purdue.edu>
2022-08-25 17:10:42 -0400

    d

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='' element={<Main />} />
+        <Route path='/kyochuldotcom' element={<Main />} />
         <Route path='/portfolio' element={<Portfolio />} />
       </Routes>
     </div>
