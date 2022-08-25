3aab881f8079903056f2cbf74ff4300c285c7456
Kyochul-Jang <jang128@purdue.edu>
2022-08-25 15:43:32 -0400

    Fix Link in simpleicon

--- src/components/SimpleIcon/SimpleIcon.jsx
+++ src/components/SimpleIcon/SimpleIcon.jsx
@@ -8,10 +8,10 @@ export default function SimpleIcon() {
             <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous" />
             <section id="set-8">
                 <div class="blurb-ripple-out-wrap hi-icon-effect-8 mt-4 links">
-                    <Link to="http://twitter.com"><i class="blurb-ripple-out fa fa-file" style={{ color: "#454839" }}>Resume</i></Link>
-                    <Link to="https://www.linkedin.com/in/kyochul-jang-93b263208/"><i class="blurb-ripple-out fa  fa-linkedin" style={{ color: "#0E76A8" }}>LinkedIn</i></Link>
-                    <Link to="https://www.instagram.com/kjang_hochul/"><i class="blurb-ripple-out fa  fa-instagram" style={{ color: "#E1306C" }}>Instagram</i></Link>
-                    <Link to="https://github.com/OfficerChul"><i class="blurb-ripple-out fa  fa-github-alt" style={{ color: "#77579d" }}>GitHub</i></Link>
+                    <a href="http://twitter.com"><i class="blurb-ripple-out fa fa-file" style={{ color: "#454839" }}>Resume</i></a>
+                    <a href="https://www.linkedin.com/in/kyochul-jang-93b263208/"><i class="blurb-ripple-out fa  fa-linkedin" style={{ color: "#0E76A8" }}>LinkedIn</i></a>
+                    <a href="https://www.instagram.com/kjang_hochul/"><i class="blurb-ripple-out fa  fa-instagram" style={{ color: "#E1306C" }}>Instagram</i></a>
+                    <a href="https://github.com/OfficerChul"><i class="blurb-ripple-out fa  fa-github-alt" style={{ color: "#77579d" }}>GitHub</i></a>
                     <Link to="portfolio"><i class="blurb-ripple-out fa  fa-star" style={{ color: "#fce903" }}>Portfolio</i></Link>
                     <a href='mailto:gcj1234567890@gmail.com'><i class="blurb-ripple-out fa  fa-envelope" style={{ color: "#368BFE" }}>Email</i></a>
                 </div>
