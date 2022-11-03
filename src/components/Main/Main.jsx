c2ed782bc82cfad58b35cb88e2149f28a1344ec2
Kyochul-Jang <jang128@purdue.edu>
2022-11-02 22:29:04 -0400

    change logo size

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,7 +6,7 @@ import './Main.css'
 export default function Searchbar() {
     return (
         <div className="main">
-            <img className="mx-auto w-25 mt-5 logo" src={logo} alt='logo' />
+            <img className="mx-auto mt-5 logo" src={logo} alt='logo' id = 'main-logo'/>
             <form action='https://www.google.com/search' method='GET'>
                 <div style={{width: '50%'}} className="mx-auto search-bar input-group mb-3" >
                     <input  name='q' type='text' className='shadow-sm form-control rounded-pill mb-3' placeholder='Search Google or type a URL'  id='search-bar'/>
