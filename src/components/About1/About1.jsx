f53a851c4392d35b67538b4b1784112ebc383522
OfficerChul <gcj1234567890@gmail.com>
2023-08-14 18:44:27 +0900

    .

--- src/components/About1/About1.jsx
+++ src/components/About1/About1.jsx
@@ -4,7 +4,6 @@ import './About1.css';
 import { Fade } from 'react-reveal';
 
 
-
 function About1({ id }) {
 
     const onButtonClick = () => {
@@ -43,13 +42,19 @@ function About1({ id }) {
                                 <br /><span className='univ'>My interests</span> lie in Computer Vision, NLP, and Brain-Computer Interface (BCI).
                             </span>
                             </h1> */}
-                            <br /><h4 className='font-mono'>Hi, I am Kyochul Jang, a Computer Science student at <span id='emphasize-about' className=''>Purdue university</span>. My concentration is Machine Learning, and I also study Mathematics as a minor.
-                                My interests lie in <span id='emphasize-about' className=''>Computer Vision, NLP, and Brain-Computer Interface(BCI)</span>.<br /><br />
-                                I am currently looking for a <span id='emphasize-about' className=''>Summer 2023 Internship</span>. <br /><br />Feel free to contact me via contacts at the <span id='emphasize-about' className=''>bottom</span> of the website!</h4>
+                            <br /><h4 className='font-mono'>Hi, I am Kyochul Jang, a Computer Science student at <span id='emphasize-about' className=''>Purdue university</span>.
+                                My interests lie in <span id='emphasize-about' className=''>Machine Learning Engineering</span> and <span id='emphasize-about'>Data Mining</span>.<br /><br />
+                                During the Summer Vacation in 2023, and 2022, I did an internship at <span id='emphasize-about' className=''>Samsung Electronics</span> and <span id='emphasize-about' className=''>Seoul National University</span> as a SDE intern.<br /><br />
+
+                                I am, now, seeking a position in a <span id='emphasize-about'>fast-paced environment</span> where I can use my skills and experience to make a real impact.
+
+                                
+                                <br /><br />Feel free to contact me via contacts at the <span id='emphasize-about' className=''>bottom</span> of the website!</h4>
                             
-                            <button onClick={onButtonClick} className='mt-3' id='download-btn'>
+                            {/* <button onClick={onButtonClick} className='mt-3' id='download-btn'>
                             <i className="fa fa-download" /> Download Resume
-                            </button>
+                            </button> */}
+                            
                             
                         </div>
 
