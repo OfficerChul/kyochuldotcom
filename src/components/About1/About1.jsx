07020a645b1e38d2575706cd3a9a0e93e6bfe86e
OfficerChul <gcj1234567890@gmail.com>
2023-08-16 17:52:37 +0900

    .

--- src/components/About1/About1.jsx
+++ src/components/About1/About1.jsx
@@ -9,13 +9,13 @@ function About1({ id }) {
     const onButtonClick = () => {
         // using Java Script method to get PDF file
         fetch('src/components/SimpleIcon/Resume_mp_no_GPA.pdf').then(response => {
-            response.blob().then(blob => { 
+            response.blob().then(blob => {
                 // Creating new object of PDF file
                 const fileURL = window.URL.createObjectURL(blob);
                 // Setting various property values
                 let alink = document.createElement('a');
                 alink.href = fileURL;
-                alink.download = 'Resume_mp_no_GPA.pdf';
+                alink.download = 'src/components/SimpleIcon/Resume_mp_no_GPA.pdf';
                 alink.click();
             })
         })
