2bc190c621e55e22e7af9c8772a79e693014564e
OfficerChul <gcj1234567890@gmail.com>
2024-06-29 05:18:16 +0900

    .

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,52 +2,40 @@ import React from 'react'
 import logo from './triangle-skyblue.png';
 import SimpleIcon from '../SimpleIcon/SimpleIcon';
 import './Main.css'
-import { Link } from 'react-router-dom';
-import MagicCard from '../MagicCard/MagicCard';
+import Navigation from '../Navigation/Navigation';
+
 
 
 export default function Searchbar() {
     return (
         <div className="main">
+            <Navigation />
             
-            <nav className="flex bg-slate-200" aria-label="Breadcrumb">
-                <ol className="inline-flex items-center space-x-1 md:space-x-3 pt-1  mb-2">
-                    <li className="inline-flex items-center aria-current='page'">
-                        <div className="flex items-center">
-                            <Link to="/" className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
-                                <svg className="text-sm font-medium text-black-500 dark:text-gray-400 w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
-                                Home
-                            </Link>
-
+            <div className="mt-20">
+                <img className="mx-auto mt-5 logo h-52 md:h-60" src={logo} alt='logo' id='main-logo' />
+                <form action='https://www.google.com/search' method='GET'>
+                    <div className="w-full flex items-center justify-center">
+                        <div className="relative w-3/4vw md:w-1/2 lg:w-2/5">
+                            <i className="fas fa-search absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"></i>
+                            <input
+                                name='q'
+                                type='text'
+                                className='shadow-custom border-none w-full rounded-full h-10 pl-10'
+                                placeholder='Search Google or type a URL'
+                                id='search-bar'
+                            />
                         </div>
-                    </li>
-                    <li>
-                        <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
-                    </li>
-                </ol>
-                <p className="pt-2 text-sm absolute top-0 right-0">This website was created entirely by myself, without the use of any pre-made themes or templates.</p>
-                
-            </nav>
+                    </div>
+                </form>
 
-            <img className="mx-auto mt-5 logo" src={logo} alt='logo' id='main-logo' />
-            <form action='https://www.google.com/search' method='GET'>
-                <div style={{ width: '50%' }} className="mx-auto search-bar input-group" >
-                    <input name='q' type='text' className='shadow-sm form-control rounded-pill card' placeholder='Search Google or type a URL' id='search-bar' />
+                <div className="menu mt-4">
+
+                    {/* <Link to="portfolio"><span className='text-sky-500 mb-3 flex justify-center'>Portfolio Website!</span><MagicCard site='portfolio-img' /></Link>
+                <a href='https://officerchul.github.io/'><span className='text-white mb-3 flex justify-center'>Jekyll Blog</span><MagicCard site='blog-img'/></a>                 */}
                 </div>
-            </form>
-          
-            
-            
-            
-            {/* <Like /> */}
-            
-            <div className="menu mt-4">
-                
-                <Link to="portfolio"><span className='text-sky-500 mb-3 flex justify-center'>Portfolio Website!</span><MagicCard site='portfolio-img' /></Link>
-                <a href='https://officerchul.github.io/'><span className='text-white mb-3 flex justify-center'>Jekyll Blog</span><MagicCard site='blog-img'/></a>                
+                <SimpleIcon />
             </div>
-            <SimpleIcon />
-            
+
 
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,21 @@ module.exports = {
   ],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        "3/4vw": "75vw",
+        "1/2vw": "50vw",
+        "1/4vw": "25vw",
+      },
+      height: {
+        "3/4vh": "75vh",
+        "1/2vh": "50vh",
+        "1/4vh": "25vh",
+      },
+      boxShadow: {
+        'custom': '3px 3px 5px rgba(0, 0, 0, 0.4)',
+      }
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
